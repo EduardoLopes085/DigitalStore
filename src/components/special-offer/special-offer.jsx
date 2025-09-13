a9ac3f826d2c041d8c6ca241ee98ff6256f98e9d
EduardoLopes085 <eduardolcb18@gmail.com>
2025-09-12 22:48:47 -0300

    criacao esqueleto padrao special-offer

---
+++ src/components/special-offer/special-offer.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import styles from './special-offer.module.css'
+
+
+function specialOffer() {
+  return (
+    <div className={styles['special-offer-container']}>
+        <div className={styles['special-offer-wrapper']}>
+
+            <div className={styles['offer-image']}>
+
+            </div>
+
+            <div className={styles['offer-text']}>
+
+            </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default specialOffer

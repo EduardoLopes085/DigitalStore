4c9c78355e1531d9558ac77dad8ccae4e612ac34
EduardoLopes085 <eduardolcb18@gmail.com>
2025-09-13 02:41:49 -0300

    estilizacao texto special-offer

--- src/components/special-offer/special-offer.jsx
+++ src/components/special-offer/special-offer.jsx
@@ -17,9 +17,11 @@ function SpecialOffer() {
 
                 <h2>Air Jordan edição de colecionador</h2>
 
-                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
-                    labore et dolore magna aliqua. 
+                <p> 
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
                     Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
+                    
                 </p>
 
                 

0b82e84acfc66911308ca7d0181080fbb9612506
EduardoLopes085 <eduardolcb18@gmail.com>
2025-09-15 22:10:25 -0300

    dinamizacao square-destach

--- src/components/square-destach/square-destach.jsx
+++ src/components/square-destach/square-destach.jsx
@@ -4,63 +4,37 @@ import DiscountAlert from '../discount-alert/discountAlert';
 import PositionButton from '../position-button/position-button';
 
 function SquareDestach() {
+  const cards = [
+    { modifier: 'square-card--supreme', desconto: '30% OFF' },
+    { modifier: 'square-card--adidas', desconto: '30% OFF' },
+    { modifier: 'square-card--bass', desconto: '30% OFF' },
+  ];
+
   return (
     <div className={styles['square-destach']}>
-        <div className={styles['square-container']}>
-            <h1 className={styles['square-title']}>
-                Coleções em Destaque
-            </h1>
+      <div className={styles['square-container']}>
+        <h2 className={styles['square-title']}>
+          Coleções em Destaque
+        </h2>
 
-            <div className={styles['square-card-container']}>
-
-                <div className={styles['square-card']}>
-                    
-                  
-                  <DiscountAlert desconto='30% OFF'/>
-
-                  <PositionButton message="comprar" 
-                  link="https://www.google.com/?hl=pt_BR&zx=1757037742483&no_sw_cr=1"
-
-                  comboClass={styles['button-configuration']}
-                  />  
-                  
-
-                </div>
-
-                <div className={styles['square-card']}>
-                    
-                  
-                  <DiscountAlert desconto='30% OFF'/>
-
-                  <PositionButton message="comprar" 
-                  link="https://www.google.com/?hl=pt_BR&zx=1757037742483&no_sw_cr=1"
-
-                  comboClass={styles['button-configuration']}
-                  />  
-                  
-
-                </div>
-
-                <div className={styles['square-card']}>
-                    
-                  
-                  <DiscountAlert desconto='30% OFF'/>
-
-                  <PositionButton message="comprar" 
-                  link="https://www.google.com/?hl=pt_BR&zx=1757037742483&no_sw_cr=1"
-
-                  comboClass={styles['button-configuration']}
-                  />  
-                  
-
-                </div>
-
+        <div className={styles['square-card-container']}>
+          {cards.map((card, index) => (
+            <div 
+              key={index} 
+              className={`${styles['square-card']} ${styles[card.modifier]}`}
+            >
+              <DiscountAlert desconto={card.desconto} />
+              <PositionButton 
+                message="comprar" 
+                link="https://www.google.com/?hl=pt_BR"
+                comboClass={styles['button-configuration']}
+              />
             </div>
-
-
+          ))}
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default SquareDestach;

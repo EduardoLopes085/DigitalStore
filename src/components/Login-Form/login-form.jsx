7ab822f5fb8370e3b25e9f1f48b4128995e42419
EduardoLopes085 <eduardolcb18@gmail.com>
2025-09-20 16:02:02 -0300

    tela de registro

--- src/components/Login-Form/login-form.jsx
+++ src/components/Login-Form/login-form.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import styles from './login-form.module.css'
 import sneaker from '../../assets/backgrounds/pexels-melvin-buezo-2529148 1.png'
 import sneaker2 from '../../assets/backgrounds/pexels-melvin-buezo-2529148 2.png'
+import gmail from '../../assets/icons/gmail_vetor.png'
+import facebook from '../../assets/icons/facebook_vetor.png'
 
 function LoginForm() {
   return (
@@ -9,52 +11,45 @@ function LoginForm() {
         <div className={styles['register-wrapper']}>
 
             <div className={styles['register-form']}>
-                <h3>Acesse sua conta</h3>
-                
                 <div>
-                    Novo cliente? Então registre-se <a href="">aqui</a>
-                </div>
-
-                <form action="#">
-                    <div className={styles['login-row']}>
-                        <label htmlFor="email-input">Login*</label>
-                        <input type="text" id='email-input' placeholder='Insira seu login ou email'/>
-                    </div>
-
-                    <div className={styles['password-row']}>
-                        <label htmlFor="password-input">Senha*</label>
-                        <input type="text" id='password-input' placeholder='Insira sua senha'/>
-                    </div>
-
-                    <a href="#" className={styles['forgot-password']} >
-                        Esqueci minha senha
-                    </a>
-
-                    <button>
-                        Acessar Conta
-                    </button>
-
+                    <h3>Acesse sua conta</h3>
+                    
                     <div>
-                        Ou faça login com
+                        Novo cliente? Então registre-se <a href="">aqui</a>
                     </div>
-                </form>
-
-                
-
-
+                    <form action="#">
+                        <div className={styles['login-row']}>
+                            <label htmlFor="email-input">Login*</label>
+                            <input type="text" id='email-input' placeholder='Insira seu login ou email'/>
+                        </div>
+                        <div className={styles['password-row']}>
+                            <label htmlFor="password-input">Senha*</label>
+                            <input type="password" id='password-input' placeholder='Insira sua senha'/>
+                        </div>
+                        <a href="#" className={styles['forgot-password']} >
+                            Esqueci minha senha
+                        </a>
+                        <button className={styles['register-button']} type='submit'>
+                            Acessar Conta
+                        </button>
+                        <div className={styles['auther-login']}>
+                            Ou faça login com
+                            <img src={gmail} alt="logo g-mail" />
+                            <img src={facebook} alt="logo facebook" />
+                        </div>
+                    </form>
+                </div>
             </div>
             
             <div className={styles['image-wrapper']}>
                 <div >
-                    <img src={sneaker} alt="" />
+                    <img src={sneaker} alt="imagem de tênis flutuante" />
                 </div>
 
-                <div>
-                    <img src={sneaker2} alt="" />
+                <div className={styles['margin-top']}>
+                    <img src={sneaker2} alt="imagem de tênis flutuante" />
                 </div>
             </div>
-
-        
 
         </div>
     </section>

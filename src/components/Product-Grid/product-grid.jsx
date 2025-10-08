bcf3a3bf110c34b7ae985e796d66d7169fe79672
EduardoLopes085 <eduardolcb18@gmail.com>
2025-10-07 23:25:21 -0300

    reponsividade mobile grid produtos

--- src/components/Product-Grid/product-grid.jsx
+++ src/components/Product-Grid/product-grid.jsx
@@ -7,11 +7,11 @@ function ProductGrid() {
   
   const sneakers = [
         { id: 0, img: sneaker, categoria: 'Tênis', name: 'K-Swiss V8', gender: 'masculino', price: 200, discountPrice: 100 },
-        { id: 1, img: sneaker, categoria: 'Tênis', name: 'Nike Revolution 6 Next Nature', gender: 'masculino', price: 319.99, discountPrice: 219.99 },
-        { id: 2, img: sneaker, categoria: 'Tênis', name: 'Asics Gel-Nimbus 25', gender: 'feminino', price: 749.99, discountPrice: 649.99 },
+        { id: 1, img: sneaker, categoria: 'Tênis', name: 'Nike Revolution', gender: 'masculino', price: 319.99, discountPrice: 219.99 },
+        { id: 2, img: sneaker, categoria: 'Tênis', name: 'Asics Gel-Nimbus', gender: 'feminino', price: 749.99, discountPrice: 649.99 },
         { id: 3, img: sneaker, categoria: 'Tênis', name: 'Reebok Nano X3', gender: 'masculino', price: 499.99, discountPrice: 449.99 },
-        { id: 4, img: sneaker, categoria: 'Tênis', name: 'Mizuno Wave Prophecy 11', gender: 'feminino', price: 999.99, discountPrice: 899.99 },
-        { id: 5, img: sneaker, categoria: 'Tênis', name: 'Adidas Ultraboost Light', gender: 'feminino', price: 599.99, discountPrice: 499.99 },
+        { id: 4, img: sneaker, categoria: 'Tênis', name: 'Mizuno Wave Prophecy', gender: 'feminino', price: 999.99, discountPrice: 899.99 },
+        { id: 5, img: sneaker, categoria: 'Tênis', name: 'Adidas Ultraboost', gender: 'feminino', price: 599.99, discountPrice: 499.99 },
         { id: 6, img: sneaker, categoria: 'Tênis', name: 'Puma RS-X', gender: 'masculino', price: 399.99, discountPrice: 349.99 }
     ];
   
